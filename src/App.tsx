02ca09309becc988c070bff0e41ee2e8bbb0e94f
Elapo <elapotrigu1@gmail.com>
2025-08-26 11:43:02 +0200

    chore: initial Vite app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <main style={{maxWidth: 900, margin: "40px auto", padding: "0 16px"}}>
+      <h1>Hello Mami Foods</h1>
+      <p>This is our starter site. We’ll plug in database and pages next.</p>
+    </main>
+  );
+}
+
+export default App
